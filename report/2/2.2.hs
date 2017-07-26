a536eb7bc6807a389c529a0524e37e3c5a4138eb
maeken2010 <maeken2010@gmail.com>
2017-07-26 16:19:45 +0900

    Update 2.2.hs
    同名の関数があったので修正

--- report/2/2.2.hs
+++ report/2/2.2.hs
@@ -11,13 +11,13 @@ x3l = \a b c d -> c
 x4l = \a b c d -> d
 
 -- 3
-x3' a b c d = x2 _ c _ _
-x4' a b c d = x3' _ _ d _
+x3_3 a b c d = x2 _ c _ _
+x4_3 a b c d = x3_3 _ _ d _
 
 -- 4
-x1' a b c d = x4' _ _ _ a
-x2' a b c d = x4' _ _ _ b
-x3' a b c d = x4' _ _ _ c
+x1_4 a b c d = x4_3 _ _ _ a
+x2_4 a b c d = x4_3 _ _ _ b
+x3_4 a b c d = x4_3 _ _ _ c
 
 -- 5
 {-
