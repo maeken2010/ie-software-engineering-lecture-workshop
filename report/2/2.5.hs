bc2aa6d3de4778ca570334f1d90c9dbb849c1887
Kentaro MAESHIRO <maeken2010@gmail.com>
2017-08-02 13:56:12 +0900

    2.5にtree例追加

--- report/2/2.5.hs
+++ report/2/2.5.hs
@@ -4,6 +4,9 @@
 data Nodes a = Node a (Nodes a) (Nodes a) | Leaf a
   deriving (Show)
 
+tree = Node 1 (Node 3 (Leaf 4) (Leaf 5)) (Leaf 6)
+treeF = Node (*2) (Node (*4) (Leaf (*5)) (Leaf (*3))) (Leaf (*10))
+
 instance Functor Nodes where
   fmap f (Leaf a) = (Leaf (f a))
   fmap f (Node a left right) = Node (f a) (fmap f left) (fmap f right)
